cca2f44ffa5e0f6c28da36af8966a97aab316fc4
Albert <me@albert.sh>
2018-09-16 06:44:42 +0200

    add wolfram->latex

--- website/flaskserver/main.py
+++ website/flaskserver/main.py
@@ -1,9 +1,21 @@
 import requests
 import urllib
+import pexpect
 
 APPID="V5KXEA-HL5T9LY4T7"
 
 def text2latex(text):
+    text=text.lower().replace('some', 'sum')
     print(text)
     r=requests.get("http://api.wolframalpha.com/v2/query?appid="+APPID+"&format=plaintext&output=json&async=0.1&input="+urllib.parse.quote_plus(text)).json()
-    return r["queryresult"]["pods"][0]["subpods"][0]["plaintext"]
+    print(r)
+    try:
+        m = r["queryresult"]["pods"][0]["subpods"][0]["plaintext"]
+        process = pexpect.spawn("wolfram -rawterm")
+        process.expect_exact(":=")
+        process.sendline('TeXForm[HoldForm['+m+']]')
+        process.expect("//TeXForm= ")
+        response = process.after.strip()
+        return response
+    except:
+        return ' '
